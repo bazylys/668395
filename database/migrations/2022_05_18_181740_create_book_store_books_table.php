ee002916b187566a3a5eea5047f5cd2f5aedc8a9
Artem Bazyliuk <bazylys.artem@gmail.com>
2022-05-18 21:22:36 +0300

    Added models and migrations

---
+++ database/migrations/2022_05_18_181740_create_book_store_books_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('book_store_books', function (Blueprint $table) {
+            $table->foreignId('book_id')->references('id')->on('books');
+            $table->foreignId('book_store_id')->references('id')->on('book_stores');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('book_store_books');
+    }
+};
